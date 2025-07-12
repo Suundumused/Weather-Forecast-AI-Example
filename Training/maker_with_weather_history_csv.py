c7a2edc71089819725b8ad8ec38966b2a192e73a
Caio Silva <caiopacheco02@gmail.com>
2025-07-11 23:40:14 -0300

    code fixed

--- Training/maker_with_weather_history_csv.py
+++ Training/maker_with_weather_history_csv.py
@@ -1,77 +1,16 @@
-import sys
 import os
-
-from threading import Thread
 import pandas as pd
 
+from threading import Thread
+from Training.Features.utils import scale_values, label_string_cells
 
-path = os.path.join(os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(sys.argv[0])), 
-    'Training', 'local_data', 'weatherHistory.csv')
 
-def scale_value(val:float) -> int:
-    val = str(val)[:10].replace('.', '') #Scales the cell value by converting it to an integer including max 10 decimal digits.
+def parse_dataframe(path:str) -> tuple[pd.DataFrame, dict]:
+    path = os.path.join(path, 'weatherHistory.csv')
     
-    while val[0] == '0':
-        val = val[1:]
-            
-    return int(float(val))
+    df = pd.read_csv(os.path.join(path))
 
-
-def scale_values(df:pd.DataFrame) -> None:
-    i=0
-    while True:
-        try:
-            df.iloc[i, 0] = scale_value(df.iloc[i, 0])
-            df.iloc[i, 1] = scale_value(df.iloc[i, 1])
-            df.iloc[i, 2] = scale_value(df.iloc[i, 2])
-            df.iloc[i, 3] = scale_value(df.iloc[i, 3])
-            df.iloc[i, 4] = scale_value(df.iloc[i, 4])
-            df.iloc[i, 5] = scale_value(df.iloc[i, 5])
-            df.iloc[i, 6] = scale_value(df.iloc[i, 6])
-            
-            i+=1
-            
-        except IndexError:
-            break
-
-
-def label_string_cells(df:pd.DataFrame, addrs_summary:dict, precips_type:dict) -> None:
-    val_index_summary = 1
-    val_precips_type = 1
-    
-    i=0
-    while True: #Converts all string labels to unique integers from the Summary and Precip Type columns.
-        try:
-            current_summary = df.iloc[i, 0]
-            current_precips_type = df.iloc[i, 1]
-                    
-            try:
-                addrs_summary[current_summary]
-                
-            except KeyError:
-                addrs_summary[current_summary] = val_index_summary
-                val_index_summary += 1
-            
-            try:
-                precips_type[current_precips_type]
-                
-            except KeyError:
-                precips_type[current_precips_type] = val_precips_type
-                val_precips_type += 1
-            
-            df.iloc[i, 0] = addrs_summary[current_summary] #Converts the text label to numeric units.
-            df.iloc[i, 1] = precips_type[current_precips_type] # ...
-            
-            i+=1
-                
-        except IndexError:
-            break
-
-
-def parse_dataframe() -> tuple[pd.DataFrame, dict]:
-    df = pd.read_csv(path)
-
-    try:
+    try: #Remove redundant and unnecessary information for training
         df.drop('Formatted Date', axis=1, inplace=True)
         df.drop('Daily Summary', axis=1, inplace=True)
         df.drop('Loud Cover', axis=1, inplace=True)
@@ -88,9 +27,7 @@ def parse_dataframe() -> tuple[pd.DataFrame, dict]:
     
     string_cols = df.select_dtypes(include=['object', 'string'])
     df = df.select_dtypes(include=['number'])
-    
-    df = (df-df.mean()) / df.std() #Normalizes class values ​​for better results
-    
+        
     process_1 = Thread(target=label_string_cells, args=(string_cols, addrs_summary, precips_type))
     process_1.start()
     
@@ -99,16 +36,13 @@ def parse_dataframe() -> tuple[pd.DataFrame, dict]:
     
     process_2.join()
     process_1.join()
+        
+    df = pd.concat([string_cols, df], axis=1) #Combines back the DataFrame with the old string columns as labels
+    df = df.convert_dtypes().astype(int)
     
-    df = pd.concat([string_cols, df], axis=1) #Combines back the DataFrame with the string columns
+    #df.to_csv(path, index=False)
     
-    df = df[df.duplicated(subset=[coloum for coloum in df.columns], keep=False)] # Identify and drop unique classes
-    df = df.convert_dtypes()
-    df = df.astype(int)
-    
-    df.to_csv(path, index=False)
-    
-    print("New table example:")
+    print("New normalized dataframe:")
     print(df.head())
     
-    return df, addrs_summary
+    return df, {value: key for key, value in addrs_summary.items()}
--- main.py
+++ main.py
@@ -1,37 +1,75 @@
+import sys
+import os
 import argparse
+
 import pandas as pd
+import json
 
-from sklearn.ensemble import HistGradientBoostingClassifier
+from threading import Thread
 
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(n_estimators=1500)
+
+
+path = os.path.join(os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(sys.argv[0])), 
+    'Training', 'local_data')
+
+def tuple_type(arg):
+    try:
+        values = [int(item) if item.isdigit() else item for item in arg.split(',')]
+        return tuple(values)
+    
+    except ValueError:
+        raise argparse.ArgumentTypeError("Invalid tuple format. Must be comma-separated integers/strings without spaces.")
+    
 
 def to_train() -> dict:
     from sklearn.model_selection import train_test_split
-    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import accuracy_score, mean_absolute_error, precision_score, recall_score, f1_score
     
-    df, labels = maker.parse_dataframe()
+    from Training.Features.utils import normalize_df
+    
+    df, labels = maker.parse_dataframe(path)
     
     print('\nNew labels for summary for prediction:')
     
+    with open(os.path.join(path, 'labels.json'), "w") as file:
+        json.dump(labels, file, indent=4)
+    
     for name, label in labels.items():
         print(f"{label}: {name}")
-        
-    plot_new_df(df)
+    
+    print('\nClose the plot window to continue...')
+    plot_new_df(normalize_df(df))
+    
+    df = df.iloc[-48:] #Using the last 48 hours of history for weather forecasting
     
     X = df.drop('Summary', axis=1)
     y = df['Summary']
     
+    '''vc = y.value_counts()
+    mask = y.isin(vc[vc >= 2].index) #Identify and drop unique classes
+
+    X = X[mask]
+    y = y[mask]'''
+    
     if len(y.unique()) > 1:
-        print(f"\nNumber of unique classes: {len(y.unique())}")
+        print(f"\nSpecific number of unique classes for y: {len(y.unique())}")
         
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
         
-    clf = HistGradientBoostingClassifier()
     clf.fit(X_train, y_train)
     
     y_pred = clf.predict(X_test)
     
     print(f"\nPrediction labels: {y_pred}")
-    print(f"\nModel trained successfully. Score: {accuracy_score(y_test, y_pred) * 100} %")
+    
+    print(f"\nModel trained successfully. Score: {accuracy_score(y_test, y_pred) * 100} %\n")
+    
+    print(f"Precision: {precision_score(y_test, y_pred, average='macro', zero_division=0) * 100:.1f} %")
+    print(f"Mean Squared Error: {mean_absolute_error(y_test, y_pred) * 100:.1f} %")
+    print(f"Recall Score: {recall_score(y_test, y_pred, average='macro') * 100:.1f} %")
+    print(f"F1 Score: {f1_score(y_test, y_pred, average='macro') * 100:.1f} %")
     
     return labels
     
@@ -43,10 +81,6 @@ def plot_new_df(df:pd.DataFrame) -> None:
     normalized_df.plot()
     
     plt.show()
-    
-    
-def predict(df:pd.DataFrame, cloud_address:str) -> pd.DataFrame:
-    return df
 
 
 if __name__ == '__main__':
@@ -57,9 +91,11 @@ if __name__ == '__main__':
     parser.add_argument('-t0', '--train_0', action='store_true', help='Train the model with the first type of dataset.')
     parser.add_argument('-t1', '--train_1', action='store_true', help='Train the model with the second type of dataset.')
     
-    parser.add_argument('-p', '--predict', type=str, default='https://any...', help='Cloud-compatible address.')
+    parser.add_argument('-p', '--predict', action='store_true', help='API-compatible address.')
     
     args = parser.parse_args()
+    
+    labels:dict = None
     
     if args.train_0:
         from Training import maker_with_weather_history_csv as maker
@@ -67,9 +103,58 @@ if __name__ == '__main__':
         labels = to_train()
         
     if args.train_1:
-        #.....Any script to parse data for the model.
         
-        labels = to_train()
+        
+        labels = to_train() #.....Any script to parse data for the model.
         
     if args.predict:
-        pass
+        from Training.Features.utils import scale_values, label_string_cells
+        import json
+        
+        print("\nSimulating a Prediction weather conditions...")    
+        
+        df = pd.DataFrame(
+                {
+                    'Summary': ["Clear"], #The correct answer will not be included in the prediction
+                    'Precip Type': ["rain"],
+                    'Temperature (C)': [13.844444444444445],
+                    'Apparent Temperature (C)': [13.844444444444445],
+                    'Humidity': [0.84],
+                    'Wind Speed (km/h)': [0.0],
+                    'Wind Bearing (degrees)': [0.0],
+                    'Visibility (km)': [15.826300000000002],
+                    'Pressure (millibars)': [1017.82]
+                }
+            )
+        
+        addrs_summary = {}
+        precips_type = {}
+        
+        string_cols = df.select_dtypes(include=['object', 'string'])
+        df = df.select_dtypes(include=['number'])
+        
+        process_1 = Thread(target=label_string_cells, args=(string_cols, addrs_summary, precips_type))
+        process_1.start()
+        
+        process_2 = Thread(target=scale_values, args=(df,))
+        process_2.start()
+        
+        process_2.join()
+        process_1.join()
+        
+        df = pd.concat([string_cols, df], axis=1) #Combines back the DataFrame with the old string columns as labels
+                
+        df = df.drop('Summary', axis=1).convert_dtypes().astype(int) #The correct answer will not be included in the prediction
+        
+        if not labels:
+            try:
+                with open(os.path.join(path, 'labels.json'), "r") as file:
+                    labels = json.load(file)
+                    
+            except FileNotFoundError:
+                print("Labels file not found. Please train the model first.")
+                sys.exit(1)
+
+        print()
+        print(df)
+        print(f"\nPrediction label: {labels[clf.predict(df)[0]]}")
